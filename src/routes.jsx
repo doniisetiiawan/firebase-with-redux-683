81a114988d70d095ba5de6218f8b9c6867d6932a
Doni Setiawan <donisetiawan.050@gmail.com>
2019-01-02 14:28:37 +0700

    Refactor routes add phrases component

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,22 +2,13 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import App from './components/App';
-import Home from './components/Home';
-import About from './components/About';
-import Coins from './components/Coins';
-import Contact from './components/Contact';
-import Notes from './components/Notes';
 import Error404 from './components/Error/404';
+import Phrases from './components/Phrases';
 
 const AppRoutes = () => (
   <App>
     <Switch>
-      <Route path="/" component={Home} exact />
-      <Route path="/about" component={About} exact />
-      <Route path="/coins" component={Coins} exact />
-      <Route path="/contact" component={Contact} exact />
-      <Route path="/notes" component={Notes} exact />
-      <Route path="/notes/:noteId" component={Notes} exact />
+      <Route path="/" component={Phrases} exact />
       <Route component={Error404} />
     </Switch>
   </App>
