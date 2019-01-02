a383084ec82716e73a7e085c00e212cf438ec647
Doni Setiawan <donisetiawan.050@gmail.com>
2019-01-02 15:22:28 +0700

    Add phrases to root reducer

--- src/shared/reducers/index.js
+++ src/shared/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 
 import device from './deviceReducer';
 import coins from '../../reducers/coinsReducer';
+import phrases from '../../reducers/phrasesReducer';
 
 const rootReducer = combineReducers({
   device,
   coins,
+  phrases,
 });
 
 export default rootReducer;
