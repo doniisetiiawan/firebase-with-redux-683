56df7ddfe387b8bd438bcf15908a9249e5e056d5
Doni Setiawan <donisetiawan.050@gmail.com>
2019-01-02 15:36:13 +0700

    Add phrases container

---
+++ src/components/Phrases/index.js
@@ -0,0 +1,30 @@
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import Phrases from './Phrases';
+
+import {
+  addPhrase,
+  deletePhrase,
+  fetchPhrases,
+  updatePhrase,
+} from '../../actions/phrasesActions';
+
+const mapStateToProps = ({ phrases }) => ({
+  phrases: phrases.phrases,
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators(
+  {
+    addPhrase,
+    deletePhrase,
+    fetchPhrases,
+    updatePhrase,
+  },
+  dispatch,
+);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Phrases);
